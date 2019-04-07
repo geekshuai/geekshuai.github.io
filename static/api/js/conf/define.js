9d81f42b75ac84606d11ef20a46d3eb40f419b02
geekshuai <1049824195@qq.com>
2019-04-07 10:49:44 +0800

    Site updated: 2019-04-07 10:49:34

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 07 2019 10:27:11 GMT+0800 (GMT+08:00)
+// build time:Sun Apr 07 2019 10:49:31 GMT+0800 (GMT+08:00)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
